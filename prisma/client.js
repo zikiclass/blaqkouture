bc101b7a4b0cfcf0f058e2a648a834708759b4ad
zikiclass <ezekieledafe5@gmail.com>
2024-10-16 08:59:49 -0700

    error handling

--- prisma/client.js
+++ prisma/client.js
@@ -6,23 +6,18 @@ let prisma;
 
 const prismaClientSingleton = () => {
   if (!prisma) {
-    prisma = new PrismaClient({
-      log: ["query", "info", "warn", "error"], // You can log Prisma queries for better debugging (optional)
-    });
+    prisma = new PrismaClient();
   }
   return prisma;
 };
 
-// Use global for sharing Prisma Client in serverless environments
-// In Vercel or other serverless platforms, the global object persists across invocations in the same environment
-// but does not across multiple function instances or cold starts, so this ensures we reuse the same Prisma Client
+// Ensure globalThis is declared properly
+global.prismaGlobal = global.prismaGlobal || prismaClientSingleton();
 
-if (process.env.NODE_ENV === "production") {
-  // In production, ensure a new client is used
-  prisma = new PrismaClient();
-} else {
-  // In development or staging, use the global singleton to avoid creating multiple instances
-  global.prisma = global.prisma || prismaClientSingleton();
+// Export prisma instance
+export default global.prismaGlobal;
+
+// Optionally set prismaGlobal in non-production environments
+if (process.env.NODE_ENV !== "production") {
+  global.prismaGlobal = prismaClientSingleton();
 }
-
-export default global.prisma; // Export the Prisma client, ensuring it's shared across requests
